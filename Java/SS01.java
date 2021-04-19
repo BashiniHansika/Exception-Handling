d419cf710f78f326f527d45b47af78b6eaf94d74
BashiniHansika <82766523+BashiniHansika@users.noreply.github.com>
2021-04-19 22:53:38 +0530

    Add files via upload

---
+++ Java/SS01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss01;
+
+/**
+ *
+ * @author Bashi
+ */
+public class SS01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try{
+            int[] myNumbers = {1,2,3};
+            System.out.println(myNumbers[10]);
+        }catch (Exception e){
+            System.out.println("Something went wrong");
+            
+        
+        }
+    }
+    
+}
